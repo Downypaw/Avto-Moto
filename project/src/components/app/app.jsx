9a7ecd107d435c7d6e57fa6df31c3a02a1c0be83
Таня <tanamalceva274@gmail.com>
2021-08-20 19:41:04 +0300

    Подготовка компонентов

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,5 +1,8 @@
-function App() {
-  return ();
-}
+import React from 'react';
+import MainPageScreen from '../main-page/main-page';
 
-export default App;
+export default function App() {
+  return (
+    <MainPageScreen />
+  );
+}
---
+++ project/src/components/main-page/main-page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../header/header';
+import GoodPresentation from '../good-presentation/good-presentation';
+import TabsSection from '../tabs-section/tabs-section';
+import Footer from '../footer/footer';
+import Popup from '../popup/popup';
+
+export default function MainPage() {
+  return (
+    <Header />
+    <GoodPresentation />
+    <TabsSection />
+    <Footer />
+    <Popup />
+  );
+}
