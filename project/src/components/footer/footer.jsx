5c0dbf1539f772c500c9e533e0383f2524fe7e66
Таня <tanamalceva274@gmail.com>
2021-08-21 08:40:55 +0300

    Добавит стили для подвала

--- project/src/components/footer/footer.jsx
+++ project/src/components/footer/footer.jsx
@@ -2,7 +2,46 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <>
-    </>
+    <footer className="page-footer">
+      <div className="container">
+        <ul className="extra-navigation">
+          <li className="extra-navigation__item">
+            <a className="extra-navigation__link" href="#">
+              Корпоративным клиентам
+            </a>
+          </li>
+          <li className="extra-navigation__item">
+            <a className="extra-navigation__link" href="#">
+              Клиентам
+            </a>
+          </li>
+          <li className="extra-navigation__item">
+            <a className="extra-navigation__link" href="#">
+              Аренда авто
+            </a>
+          </li>
+          <li className="extra-navigation__item">
+            <a className="extra-navigation__link" href="#">
+              Каршеринг
+            </a>
+          </li>
+          <li className="extra-navigation__item">
+            <a className="extra-navigation__link" href="#">
+              Как продать авто
+            </a>
+          </li>
+          <li className="extra-navigation__item">
+            <a className="extra-navigation__link" href="#">
+              Trade-in
+            </a>
+          </li>
+          <li className="extra-navigation__item">
+            <a className="extra-navigation__link" href="#">
+              Test drive
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
 }
