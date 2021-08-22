35d17602c5857e06e0120b2040dc520785d8dcfd
Таня <tanamalceva274@gmail.com>
2021-08-22 21:16:48 +0300

    Добавит стили для блока описания товара

--- project/src/components/good-information/good-information.jsx
+++ project/src/components/good-information/good-information.jsx
@@ -4,7 +4,7 @@ export default function GoodInformation() {
   return (
     <div className="good-information">
       <h2 className="good-information__name">Марпех 11</h2>
-      <dl className="good-information__main-features main-features">
+      <dl className="main-features">
         <dt className="main-features__feature visually-hidden">Тип двигателя</dt>
         <dd className="main-features__value">Бензин</dd>
         <dt className="main-features__feature visually-hidden">Коробка передачь</dt>
@@ -15,8 +15,8 @@ export default function GoodInformation() {
         <dd className="main-features__value">1.4 л</dd>
       </dl>
       <div className="good-information__price">
-        <span className="good-information__new-price">2 300 000 ₽</span>
-        <span className="good-information__old-price">2 400 000 ₽</span>
+        <pre className="good-information__new-price">2 300 000 ₽</pre>
+        <pre className="good-information__old-price">2  400 000 ₽</pre>
       </div>
       <button className="good-information__request">Оставить заявку</button>
       <button className="good-information__credit">В кредит от 11 000 ₽</button>
